6c80d97850bb5ac5ba61484f0244e21a4371adb2
BOULET Pierre <pierre.boulet@maif.fr>
2026-05-12 11:18:10 +0200

    feat: created Shoes class, updated Clothing with override method

--- src/myClasses/Shoes.ts
+++ src/myClasses/Shoes.ts
@@ -1,10 +1,10 @@
-import type { ClothingSize } from "../types/ClothingSize.js";
+import { ShoeSize } from "../types/ShoeSize.js";
 import { Product } from "./Product.js";
 
-export class Clothing extends Product {
- size: ClothingSize;
+export class Shoes extends Product {
+ size: ShoeSize;
 
- constructor(productId: number, name: string, weight: number, price: number, size: ClothingSize) {
+ constructor(productId: number, name: string, weight: number, price: number, size: ShoeSize) {
   super(productId, name, weight, price);
   this.size = size;
  }
@@ -12,5 +12,4 @@ export class Clothing extends Product {
  displayDetails(): string {
   return `Product ID: ${this.productId}, Name: ${this.name}, Size: ${this.size}, Weight: ${this.weight}, Price: ${this.price}€`
  }
-
 }
